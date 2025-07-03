b115c03d245fa52f2d4316753866c384dfa56837
DESKTOP-HP3HHEO\acer <dviha7@gmail.com>
2025-07-03 11:42:03 +0530

    feat(models): add User and Task schemas with MongoDB connection

--- todobackend/index.js
+++ todobackend/index.js
@@ -6,10 +6,31 @@ const bcrypt = require("bcryptjs");
 const PORT = process.env.PORT || 8080;
 const MONGOURL = process.env.MONGOURL;
 const cors = require("cors");
+dotenv.config()
 
 app.use(express.json());
-
 app.use(cors());
+
+mongoose.connect(MONGOURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+  });
+  
+const User = mongoose.model("User", userSchema);
+
+const taskSchema = new mongoose.Schema({
+    text: String,
+    status: String,
+    priority: String,
+    userId: mongoose.Schema.Types.ObjectId,
+  });
+ 
+const Task = mongoose.model("Task", taskSchema);
 
 app.get("/", (req, res) => {
     res.send(" Backend is working!");
