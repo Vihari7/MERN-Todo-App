2b57791b4c50ca4821c47a8754d84a2c094f0ffb
DESKTOP-HP3HHEO\acer <dviha7@gmail.com>
2025-07-03 15:00:34 +0530

    feat(frontend): add login page with authentication flow and set up routing for login/signup

--- todofrontend/src/App.js
+++ todofrontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
 import './App.css';
 
-function App() {
+export default function App() {
+  const MainApp = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+        
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+
